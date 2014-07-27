af17dec5e9da58fe46029a6a98535e4c21d4ac72
James Brooks <jbrooksuk@me.com>
2014-07-27 22:56:39 +0100

    Changed pass

--- app/config/database.php
+++ app/config/database.php
@@ -51,7 +51,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'anorak',
 			'username'  => 'anorak',
-			'password'  => 'btbcwfibtywc!',
+			'password'  => 'btbcwfibtbywc!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
