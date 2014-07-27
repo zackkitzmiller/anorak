2e8eb023b82ba89be88fed6ea7f0337c100ad01c
James Brooks <jbrooksuk@me.com>
2014-07-27 18:47:28 +0100

    Downloading the diff file contents, but it's a start

--- app/controllers/BuildController.php
+++ app/controllers/BuildController.php
@@ -1,8 +1,19 @@
 <?php 
 
+	use \Github\Client as GithubClient;
+
 	class BuildController extends BaseController {
+		protected $Client;
+
+		public function __construct(GithubClient $Client) {
+			$this->Client = $Client;
+			$this->Client->authenticate($_ENV['ANORAK_GITHUB_TOKEN'], NULL, GithubClient::AUTH_HTTP_TOKEN);
+		}
+
 		public function build(Repo $Repo) {
-			$Payload = Input::get('payload');
+			list($User, $RepoName) = explode('/', $Repo->full_github_name);
+
+			$Payload = json_decode(Request::getContent(), TRUE);
 			$Event   = Request::header('X-GitHub-Event');
 
 			// It's a test (ping), say hi.
@@ -10,12 +21,51 @@
 				return Response::make(array(
 					'ping' => 'OK'
 				));
-			}elseif($Event === 'pull_request'){
-				if($Payload['changed_files'] < $_ENV['CHANGED_FILES_THRESHOLD']) {
-					// Queue::push('SmallBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'high');
-				}else{
-					// Queue::push('LargeBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'medium');
-				}
+			}
+			
+			// return $Payload['pull_request'];
+
+			// Get the files changed by the pull request.
+			// @TODO: Parse these files and keep the "violations"
+			$Files = $this->Client->api('pull_request')->files($User, $RepoName, $Payload['number']);
+			foreach($Files as $File) {
+				$FileName = $File['filename'];
+				$SHA      = $File['sha'];
+
+				/*$ContentsURL = 'repos/'.rawurlencode($User).'/'.rawurlencode($RepoName).'/contents/'.$FileName;
+				$this->Client->getHttpClient()->setHeaders(['Accept' => 'application/vnd.github.v3.raw']);
+				$FileRequest = $this->Client->getHttpClient()->get($ContentsURL, array(
+		            'ref' => $Payload['pull_request']['head']['ref']
+		        ));
+		        $FileContents = Github\HttpClient\Message\ResponseMediator::getContent($FileRequest);
+
+				return $FileContents;*/
+
+				// return $Payload['pull_request'];
+
+				$FileContents = $this->Client->api('repos')->contents()->download($User, $RepoName, $FileName, $Payload['pull_request']['head']['ref']);
+
+				return $FileContents;
+
+				// dd(base64_decode($FileContents['content']));
+			}
+
+			try {
+				$this->Client->api('pull_request')->comments()->create('jbrooksuk', $RepoName, $Payload['number'], array(
+					// 'pull_request_number' => $Payload['number'],
+					'body'                => 'Testing replying on a commit from Anorak!',
+					'commit_id'           => $Payload['pull_request']['head']['sha'],
+					'path'                => 'app/jobs/LargeBuildJob.php',
+					'position'            => 2
+				));
+			} catch (Exception $e) {
+				Log::error($e);
+			}
+
+			if($Payload['pull_request']['changed_files'] < $_ENV['CHANGED_FILES_THRESHOLD']) {
+				// Queue::push('SmallBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'high');
+			}else{
+				// Queue::push('LargeBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'medium');
 			}
 		}
 	}
