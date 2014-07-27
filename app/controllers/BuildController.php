8a85dc74754e631e7ffaba7546d5e3457b86e684
James Brooks <jbrooksuk@me.com>
2014-07-27 21:50:02 +0100

    Builds are working, SWEET!

--- app/controllers/BuildController.php
+++ app/controllers/BuildController.php
@@ -1,7 +1,7 @@
 <?php 
 
 	use \Github\Client as GithubClient;
-	use \PHP_CodeSniffer as Sniffer;
+	use \PHP_CodeSniffer_CLI;
 
 	class BuildController extends BaseController {
 		protected $Client;
@@ -36,29 +36,82 @@
 				$TMPFileName = '/tmp/' . date('U') . sha1($FileName) . '.cs.php';
 				file_put_contents($TMPFileName, $FileContents);
 
-				$Sniff = new Sniffer;
-				$Results = $Sniff->process($TMPFileName, 'Generic');
-				dd($Results);
+				$phpcs    = new PHP_CodeSniffer_CLI;
+				$standard = 'PSR1';
+				$files    = array($TMPFileName);
+				$ignored  = array();
 
-				try {
+				$options = array(
+					'standard'      => array($standard),
+					'files'         => $files,
+					'ignored'       => $ignored,
+					'verbosity'     => 0,
+					// 'extensions' => array('php'),
+					'restrictions'  => array(
+						// 'Generic.Files.LineLength' => 10
+					),
+					'reportWidth' => 500
+				);
+
+				ob_start();
+				$numErrors = $phpcs->process($options);
+				$Errors = ob_get_contents();
+				unlink($TMPFileName);
+				ob_end_clean();
+
+				$Violations = $this->parseResults($Errors);
+				if(count($Violations) === 0) continue;
+
+				foreach($Violations as $Violation) {
+					// $Msg = ucwords($Violation['type']) . ': ' . $Violation['message'];
+					$Msg = $Violation['message'];
+
+					// Store the violation.
+					$Build = new Build;
+					$Build->violations = $Msg;
+					$Build->repo_id = $Repo->id;
+					$Build->uuid = $Payload['pull_request']['head']['sha'];
+					$Build->save();
 
 					$this->Client->api('pull_request')->comments()->create('jbrooksuk', $RepoName, $Payload['number'], array(
-						// 'pull_request_number' => $Payload['number'],
-						'body'                => 'Testing replying on a commit from Anorak!',
+						'body'                => $Msg,
 						'commit_id'           => $Payload['pull_request']['head']['sha'],
 						'path'                => $FileName,
-						'position'            => 4
+						'position'            => $Violation['line']
 					));
-				} catch (Exception $e) {
-					Log::error($e);
 				}
 			}
-
 
 			if($Payload['pull_request']['changed_files'] < $_ENV['CHANGED_FILES_THRESHOLD']) {
 				// Queue::push('SmallBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'high');
 			}else{
 				// Queue::push('LargeBuildJob', array('payload' => $Payload, 'repo_id' => $Repo->id), 'medium');
 			}
+		}
+
+		public function parseResults($Result) {
+			$Results = preg_split('/\n/', $Result);
+
+			// Get rid of the excess
+			$Results = array_where($Results, function($key, $value) {
+				return (int)$key >= 5 && (int)$key <= 24;
+			});
+
+			// Now loop each row, save the line, type and message.
+			$Violations = array();
+			foreach($Results as $Result) {
+				$Violation = explode('|', trim($Result));
+
+				if(empty(trim($Violation[0]))) continue;
+				if(!isset($Violation[0], $Violation[1], $Violation[2])) continue;
+
+				$Violations[] = array(
+					'line'    => trim($Violation[0]),
+					'type'    => strtolower(trim($Violation[1])),
+					'message' => trim($Violation[2])
+				);
+			}
+
+			return $Violations;
 		}
 	}
