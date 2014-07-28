32ab3c06a6476a52239b5c087cf11d9778a10980
James Brooks <jbrooksuk@me.com>
2014-07-28 11:43:09 +0100

    Fixed bad PHP

--- app/config/social.php
+++ app/config/social.php
@@ -2,8 +2,8 @@
 
 	return array(
 		'github' => array(
-			'clientId'     => getenv['GITHUB_CLIENT_ID'),
-			'clientSecret' => getenv['GITHUB_CLIENT_SECRET'),
+			'clientId'     => getenv('GITHUB_CLIENT_ID'),
+			'clientSecret' => getenv('GITHUB_CLIENT_SECRET'),
 			'redirectUri'  => url('/auth/github/callback'),
 			'scopes'       => 'user:email repo'
 		),
