7695bb8cb3443d761f73286d23423dd522d289ac
James Brooks <james@bluebaytravel.co.uk>
2014-08-05 09:54:04 +0100

    Some .gitignore changes

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Jammy.local'),
+	'local' => array('Jammy.local', 'James-iMac.local'),
 
 ));
 
