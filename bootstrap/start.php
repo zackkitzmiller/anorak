0ec650f042a062e94b93b3261bc7a4b67f5cccb4
James Brooks <jbrooksuk@me.com>
2014-08-06 14:56:15 +0100

    Added homestead to local env list

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Jammy.local', 'James-iMac.local'),
+	'local' => array('Jammy.local', 'James-iMac.local', 'homestead'),
 
 ));
 
