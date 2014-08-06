dbc293f79f2177752f9dbc3e94bbd68e39e85ddc
James Brooks <jbrooksuk@me.com>
2014-08-06 14:55:59 +0100

    Switching to Homestead for local development

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => 'localhost',
 			'database'  => 'anorak',
-			'username'  => 'root',
-			'password'  => 'root',
+			'username'  => 'homestead',
+			'password'  => 'secret',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
