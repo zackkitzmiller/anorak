50aa241bfc5d57700e351e5d064fd60ca4d0362b
James Brooks <jbrooksuk@me.com>
2014-08-10 08:02:29 +0100

    Beanstalkd split into high and low (default high)

--- app/config/queue.php
+++ app/config/queue.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'default' => 'beanstalkd',
+	'default' => 'high',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -34,33 +34,18 @@ return array(
 			'driver' => 'sync',
 		),
 
-		'beanstalkd' => array(
+		'high' => array(
 			'driver' => 'beanstalkd',
 			'host'   => 'localhost',
-			'queue'  => 'default',
+			'queue'  => 'high',
 			'ttr'    => 60,
 		),
 
-		'sqs' => array(
-			'driver' => 'sqs',
-			'key'    => 'your-public-key',
-			'secret' => 'your-secret-key',
-			'queue'  => 'your-queue-url',
-			'region' => 'us-east-1',
-		),
-
-		'iron' => array(
-			'driver'  => 'iron',
-			'host'    => 'mq-aws-us-east-1.iron.io',
-			'token'   => 'your-token',
-			'project' => 'your-project-id',
-			'queue'   => 'your-queue-name',
-			'encrypt' => true,
-		),
-
-		'redis' => array(
-			'driver' => 'redis',
-			'queue'  => 'default',
+		'low' => array(
+			'driver' => 'beanstalkd',
+			'host'   => 'localhost',
+			'queue'  => 'low',
+			'ttr'    => 60,
 		),
 
 	),
