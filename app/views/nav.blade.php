5116eec068d13d8eaaca53c07f8326a2cf615c5e
James Brooks <jbrooksuk@me.com>
2014-08-15 22:19:34 +0100

    Resize logo image to fit max-width (no @2x version)

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -1,9 +1,9 @@
+<img src='/images/AnorakFull.png' class='logo' alt='Anorak' />
 <ul class="nav nav-pills pull-right">
 	@if(Auth::check())
 	<li><a href="/user">Repositories</a></li>
 	<li><a href="/user/setup">Setup</a></li>
 	@else
-	<li style='margin-top: 15px;'><a class="btn btn-lg btn-green" href="/auth/github" role="button"><i class='fa fa-github'></i> Sign in with GitHub</a></li>
+	<li class='login-btn'><a class="btn btn-lg btn-green" href="/auth/github" role="button"><i class='fa fa-github'></i> Sign in with GitHub</a></li>
 	@endif
 </ul>
-<img src='/images/AnorakFull.png' style='max-width: 420px' alt='Anorak' />
