9ff5fb358ce01f1e0fd6119d0bca785aead6421d
James Brooks <jbrooksuk@me.com>
2014-08-18 23:00:12 +0100

    Fix up the nav Anorak link

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -2,7 +2,8 @@
 	<div class="container">
 		<div class="navbar-header page-scroll">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-			<a class="navbar-brand" href="#page-top">Anorak</a> </div>
+			<a class="navbar-brand" href="/">Anorak</a>
+		</div>
 		
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav">
