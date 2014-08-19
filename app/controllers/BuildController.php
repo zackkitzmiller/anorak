cfc596040ee34d322755c69c917295f32b6964c1
James Brooks <james@bluebaytravel.co.uk>
2014-08-19 11:51:05 +0100

    Switch to using PHPCheckstyle if possible (using this as a test in itself)

--- app/controllers/BuildController.php
+++ app/controllers/BuildController.php
@@ -1,7 +1,7 @@
 <?php 
 
 	use \Github\Client as GithubClient;
-	use \PHP_CodeSniffer_CLI;
+	use \PHPCheckstyle;
 	use Symfony\Component\Yaml\Parser as YamlParser;
 
 	class BuildController extends BaseController {
@@ -25,7 +25,7 @@
 				));
 			}
 
-			if($Payload['action'] !== 'opened' || $Payload['action'] !== 'synchronize') {
+			if(!in_array($Payload['action'], array('opened', 'synchronize'))) {
 				return Response::make(array(
 					'success' => FALSE,
 					'sniffed' => FALSE
@@ -60,40 +60,21 @@
 				try {
 					$FileContents = $this->Client->api('repo')->contents()->download($User, $RepoName, $FileName, $Payload['pull_request']['head']['ref']);
 				} catch (Exception $e) {
-					return Response::make(array(
+					/*return Response::make(array(
 						'error' => TRUE,
 						'message' => 'Something went wrong when fetching "'.$User.'/'.$RepoName.'/'.$FileName.'" contents.'
-					));
+					));*/
+					continue;
 				}
 
 				$TMPFileName = storage_path() . '/files/' . $File['sha'] . '.cs.php';
 				file_put_contents($TMPFileName, $FileContents);
 
-				$phpcs    = new PHP_CodeSniffer_CLI;
-				// $phpcs->reporting = new PHP_CodeSniffer_Reports_Json;
-				$standard = array_get($BuildConfig, 'standard', 'PSR2');
-				$files    = array($TMPFileName);
+				$Style = new PHPCheckstyle(array('array'), NULL, $BuildConfig, NULL, FALSE, FALSE);
+				$Violations = $Style->processFiles($TMPFileName);
 
-				$options = array(
-					'standard'      => array($standard),
-					'files'         => $TMPFileName,
-					'ignored'       => array(),
-					'verbosity'     => 1,
-					// 'extensions' => array('php'),
-					'restrictions'  => array(
-						// 'Generic.Files.LineLength' => 10
-					),
-					'reportWidth' => 1024
-				);
+				dd($Violations);
 
-				// TODO: Replace this with whatever is used in #19
-				ob_start();
-				$phpcs->process($options, $standard);
-				$Errors = ob_get_contents();
-				unlink($TMPFileName);
-				ob_end_clean();
-
-				$Violations = $this->parseResults($Errors);
 				if(count($Violations) === 0) continue;
 
 				foreach($Violations as $LineNo => $Violation) {
@@ -118,27 +99,5 @@
 			return Response::make(array(
 				'success' => TRUE
 			));
-		}
-
-		public function parseResults($Result) {
-			$Results = preg_split('/\n/', $Result);
-
-			// Get rid of the excess
-			$Results = array_where($Results, function($key, $value) {
-				return (int)$key >= 5 && (int)$key <= 24;
-			});
-
-			// Now loop each row, save the line, type and message.
-			$Violations = array();
-			foreach($Results as $Result) {
-				$Violation = explode('|', trim($Result));
-
-				if(empty(trim($Violation[0]))) continue;
-				if(!isset($Violation[0], $Violation[1], $Violation[2])) continue;
-
-				$Violations[trim($Violation[0])][] = trim($Violation[2]);
-			}
-
-			return $Violations;
 		}
 	}
