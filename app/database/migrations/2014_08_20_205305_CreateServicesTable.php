4003b307b9f3ed290a529208d43487f23cf1b95e
James Brooks <jbrooksuk@me.com>
2014-08-20 21:54:02 +0100

    Added migration for services table

---
+++ app/database/migrations/2014_08_20_205305_CreateServicesTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('services', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->nullable(FALSE);
+			$table->string('github_username')->nullable();
+			$table->string('bitbucket_username')->nullable();
+			$table->string('gitlab_username')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('services');
+	}
+
+}
