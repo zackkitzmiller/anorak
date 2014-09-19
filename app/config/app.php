0eebbea410d58e283e4d60bbcf8e7dfe2f77e2d9
James Brooks <james@bluebaytravel.co.uk>
2014-09-19 10:23:28 +0100

    Installed Cashier and setup Stripe/Cashier migration

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Anorak\Tracking\TrackingServiceProvider',
+		'Laravel\Cashier\CashierServiceProvider',
 
 	),
 
--- app/models/User.php
+++ app/models/User.php
@@ -4,6 +4,8 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Laravel\Cashier\BillableTrait;
+use Laravel\Cashier\BillableInterface;
 
 /**
  * User Model
@@ -13,11 +15,12 @@ use Illuminate\Auth\Reminders\RemindableInterface;
  */
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, BillableTrait;
 
 	protected $table = 'users';
 	protected $hidden = array('remember_token');
 	protected $fillable = array('email_address');
+	protected $dates = array('trial_ends_at', 'subscription_ends_at');
 
 	public function memberships() {
 		return $this->hasMany('Membership', 'user_id', 'id');
