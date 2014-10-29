413cd624f81449ae7e23800e018c27d28fdb052e
James Brooks <jbrooksuk@me.com>
2014-10-29 22:22:52 +0000

    Better landing page. It's really swanky!

---
+++ app/views/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class='grey'>
+	<div class='container'>
+		<div class='row centered'>
+			<a class='btn btn-trans' href='https://twitter.com/anorakci' target="_blank"><i class='fa fa-twitter'></i> We're on Twitter</a>
+			<hr class="aligncenter mb">
+			<p>Anorak &copy; <a href='http://james-brooks.uk' target="_blank">James Brooks</a> {{ date('Y') }}</p>
+		</div>
+	</div>
+</footer>
