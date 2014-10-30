7084f877d5cbf7fce3e8f9aa8d60dc84e162cd86
James Brooks <james@bluebaytravel.co.uk>
2014-10-30 14:10:08 +0000

    GoSquared integration

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,10 @@
 (function() {
+	!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+	arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+	d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+	insertBefore(d,q)}(window,document,'script','_gs');
+	_gs('GSN-279675-G');
+
 	/**
 	 * This file should only be used for logged in users.
 	 */
