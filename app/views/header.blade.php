70cb4ba15e27d0134ecfadf8d56934a591fc6445
James Brooks <james@bluebaytravel.co.uk>
2014-10-30 13:31:57 +0000

    Fix the header hr

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -3,11 +3,8 @@
 		<div class="row centered">
 			<div class="col-md-8 col-md-offset-2">
 				<h1>Anorak</h1>
-				<hr class="aligncenter">
+				<hr class="align-center">
 				<h2>Review your PHP Pull Requests with style.</h2>
-			</div>
-			<div class="go aligncenter">
-				<a href="#about" class="smoothScroll"><i class="ion-mouse"></i></a>
 			</div>
 		</div>
 	</div>
