6e25532759d98022e32fc68a9ef4c0a5ddb06358
James Brooks <jbrooksuk@me.com>
2014-11-06 18:59:35 +0000

    SEO changes

--- app/views/html-head.blade.php
+++ app/views/html-head.blade.php
@@ -5,9 +5,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!-- Set the viewport width to device width for mobile -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Anorak is a continuous integration service for checking PHP code">
-	<meta name="keywords" content="anorak, ci, php, github, pull request, hound">
-	<meta name="author" content="james-brooks.uk">
+	<meta name="description" content="Anorak is a continuous integration service for checking PHP code. AnorakCI automatically comments on GitHub Pull Requests.">
+	<meta name="keywords" content="anorak, ci, php, github, pull request, houndci, anorakci, code, standards, service">
+	<meta name="author" content="http://james-brooks.uk">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<title>Anorak</title>
 	<link rel='stylesheet' href='/stylesheets/css/bootstrap.css' />
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,7 +10,7 @@
 		ga('create', 'UA-42158883-3', 'auto');
 		ga('send', 'pageview');
 	</script>
-	
+
 	@include('header')
 
 	@section('content')
