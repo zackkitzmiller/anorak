4aef27e9b6264959575b5d1c99e67227024c8f45
James Brooks <james@bluebaytravel.co.uk>
2014-11-12 08:31:46 +0000

    Added link to blog

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -2,6 +2,7 @@
 	<div class='container'>
 		<div class='row centered'>
 			<a class='btn btn-trans' href='https://twitter.com/anorakci' target="_blank"><i class='fa fa-twitter'></i> We're on Twitter</a>
+			<a class='btn btn-trans' href='https://blog.anorakci.com'><i class='fa fa-rss'></i> Blog</a>
 			<hr class="align-center mb">
 			<p>Anorak &copy; <a href='http://james-brooks.uk' target="_blank">James Brooks</a> {{ date('Y') }}</p>
 		</div>
