08a91f73acb3c0ce495f62549feafbebd92cf40c
James Brooks <james@bluebaytravel.co.uk>
2014-11-21 14:41:42 +0000

    Added GoSquared event tracking to footer buttons

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class='grey'>
 	<div class='container'>
 		<div class='row centered'>
-			<a class='btn btn-trans' href='https://twitter.com/anorakci' target="_blank"><i class='fa fa-twitter'></i> We're on Twitter</a>
-			<a class='btn btn-trans' href='https://blog.anorakci.com'><i class='fa fa-rss'></i> Blog</a>
+			<a class='btn btn-trans' href='https://twitter.com/anorakci' target="_blank" onclick="javascript: _gs('event', 'twitter-button')"><i class='fa fa-twitter'></i> We're on Twitter</a>
+			<a class='btn btn-trans' href='https://blog.anorakci.com' onclick="javascript: _gs('event', 'blog-button')"><i class='fa fa-rss'></i> Blog</a>
 			<hr class="align-center mb">
-			<p>Anorak &copy; <a href='http://james-brooks.uk' target="_blank">James Brooks</a> {{ date('Y') }}</p>
+			<p>Anorak &copy; <a href='http://james-brooks.uk' onclick="javascript: _gs('event', 'jamesbrooks-button')" target="_blank">James Brooks</a> {{ date('Y') }}</p>
 		</div>
 	</div>
 </footer>
