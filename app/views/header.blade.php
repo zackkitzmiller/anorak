e3c949111af8982215b4bcb8bb6b97892fc18812
James Brooks <jbrooksuk@me.com>
2014-11-23 17:43:57 +0000

    Login button when dev

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -5,6 +5,9 @@
 				<h1>Anorak</h1>
 				<hr class="align-center">
 				<h2>Review your PHP Pull Requests with style.</h2>
+				@if(Config::get('app.debug'))
+				<a href='/auth/github' class='btn btn-green'><i class='fa fa-github'></i> Signin with GitHub</a>
+				@endif
 			</div>
 		</div>
 	</div>
