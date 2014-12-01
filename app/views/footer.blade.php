f4590c39755b909db49086949e45485479d502b5
James Brooks <james@bluebaytravel.co.uk>
2014-12-01 16:42:21 +0000

    Use unsecure blog link

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class='container'>
 		<div class='row centered'>
 			<a class='btn btn-trans' href='https://twitter.com/anorakci' target="_blank" onclick="javascript: _gs('event', 'twitter-button')"><i class='fa fa-twitter'></i> We're on Twitter</a>
-			<a class='btn btn-trans' href='https://blog.anorakci.com' onclick="javascript: _gs('event', 'blog-button')"><i class='fa fa-rss'></i> Blog</a>
+			<a class='btn btn-trans' href='http://blog.anorakci.com' onclick="javascript: _gs('event', 'blog-button')"><i class='fa fa-rss'></i> Blog</a>
 			<hr class="align-center mb">
 			<p>Anorak &copy; <a href='http://james-brooks.uk' onclick="javascript: _gs('event', 'jamesbrooks-button')" target="_blank">James Brooks</a> {{ date('Y') }}</p>
 		</div>
